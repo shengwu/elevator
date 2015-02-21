56a866852ced5679b2dc224e65620a4f3d818cb9
Sheng Wu <pkernoobie@gmail.com>
2015-02-21 10:20:28 -0800

    fails challenge 4, has bugs

--- elevator.js
+++ elevator.js
@@ -1,8 +1,8 @@
 // An effective general elevator program
+// TODO: make this work better for multiple elevators
 {
     init: function(elevators, floors) {
         // Global state
-        // TODO: this only works for one elevator
         var needsUp = []; // Whether someone at floor i wants to go up
         var needsDown = [];
         for (var i = 0; i < floors.length; i++) {
@@ -10,19 +10,129 @@
             needsDown.push(false);
         }
 
-        // Assuming we start at floor 0
-        var goingUp = true;
+        var minFloor = 0;
+        var maxFloor = floors.length - 1;
+
+        // Updates an elevator's indicators
+        function updateIndicators(goingUp, elevator) {
+            elevator.goingUpIndicator(goingUp);
+            elevator.goingDownIndicator(!goingUp);
+        }
 
         // Program all elevators
         elevators.map(function(elevator) {
+            // Assuming we start at floor 0
+            var goingUp = true;
+
+            // The point at which we don't stop to pick up anyone new
+            var maxLoad = (elevator.maxPassengerCount() - 1) /
+                elevator.maxPassengerCount();
+            var personLoad = 1 / elevator.maxPassengerCount(); // average weight
+
+            // Change direction
+            function reverseDirection() {
+                goingUp = !goingUp;
+                updateIndicators(goingUp, elevator);
+            }
+
+            // In the direction that the elevator is going, pick up the next
+            // waiting person and return true.
+            // If there is noone waiting, return false.
+            function getNextPersonInCurrentDir() {
+                var dirDelta = goingUp ? 1 : -1;
+                var stop = goingUp ? maxFloor + 1 : minFloor - 1;
+                var waiting = goingUp ? needsUp : needsDown;
+                for (var i = elevator.currentFloor(); i != stop; i += dirDelta) {
+                    if (waiting[i]) {
+                        elevator.goToFloor(i, true);
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            // This is called when noone above us needs to go up, or noone below
+            // us needs to go down.
+            //
+            // In this case, we go to the furthest person in the current direction
+            // that wants to go the opposite direction. Then we pick them up and
+            // switch direction.
+            function getLastPersonInCurrentDirAndReverse() {
+                // Get the floor of the furthest person from current pos
+                // (start at 0 or maxFloor)
+                var start = goingUp ? maxFloor : minFloor;
+                var dirDelta = goingUp ? -1 : 1;
+                var waiting = goingUp ? needsDown : needsUp;
+                var destination = -1;
+                for (var i = start; i != elevator.currentFloor(); i += dirDelta) {
+                    if (waiting[i]) {
+                        destination = i;
+                        break;
+                    }
+                }
+                // If we didn't find anyone, just reverse direction now
+                if (destination === -1) {
+                    reverseDirection();
+                    getNextPersonInCurrentDir();
+                } else {
+                    // Go to that floor and reverse direction
+                    elevator.goToFloor(destination, true);
+                    reverseDirection();
+                }
+            }
+
+            // One-time thing
+            var gameStarted = false;
+
             elevator.on("idle", function() {
-                // The elevator is idle - shouldn't happen?
+                console.log('idle was called');
+                // Start the game
+                if (!gameStarted) {
+                    updateIndicators(goingUp, elevator);
+                    elevator.goToFloor(1);
+                    gameStarted = true;
+                }
+                // The elevator is idle - i.e. the destination queue is empty
+                if (goingUp && elevator.currentFloor() === maxFloor) {
+                    reverseDirection();
+                } else if (!goingUp && elevator.currentFloor() === minFloor) {
+                    reverseDirection();
+                }
+                // Check if anyone is waiting in the current direction
+                if (!getNextPersonInCurrentDir()) {
+                    getLastPersonInCurrentDirAndReverse();
+                }
             });
             elevator.on("floor_button_pressed", function(floorNum) {
+                // Update destinations based on where people want to go
+                elevator.destinationQueue = elevator.getPressedFloors();
+                elevator.checkDestinationQueue();
             });
             elevator.on("passing_floor", function(floorNum) {
+                console.log('passing_floor was called');
+                // Triggered slightly before the elevator will pass a floor
+                // If we're not planning to stop here, see if we want to
+                if (elevator.destinationQueue.indexOf(floorNum) === -1) {
+                    if (goingUp) {
+                        if (needsUp[floorNum] && elevator.loadFactor() <= maxLoad) {
+                            elevator.goToFloor(floorNum, true);
+                        }
+                    } else {
+                        if (needsDown[floorNum] && elevator.loadFactor() <= maxLoad) {
+                            elevator.goToFloor(floorNum, true);
+                        }
+                    }
+                }
             });
             elevator.on("stopped_at_floor", function(floorNum) {
+                console.log('stopped_at_floor was called');
+                // Clear needsUp or needsDown
+                // Passengers will re-press the button if they can't get in
+                if (goingUp) {
+                    needsUp[floorNum] = false;
+                } else {
+                    needsDown[floorNum] = false;
+                }
             });
         });
 
@@ -36,6 +146,7 @@
             });
         });
     },
+
     update: function(dt, elevators, floors) {
         // This is called during the challenge
         // dt is num of game seconds elapsed since last update
